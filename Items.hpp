a7aa1f49fe7933fd71e38bc5efd99210c1f0af44
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 16:52:57 -0400

    Update Items.hpp

--- Items.hpp
+++ Items.hpp
@@ -7,13 +7,17 @@
 #ifndef ITEMS_HPP
 #define ITEMS_HPP
 
+#include <string>
+
 class Items
 {
 	protected:
+		std::string type;
 	public:
 		Items();
 		~Items();
 		virtual void use() = 0;
+		std::string getType();
 };
 
 #endif
