e8b40396e7765701b1af2fe50dcc32f3d35b99d9
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:47:51 -0400

    Create Shield.cpp

---
+++ Shield.cpp
@@ -0,0 +1,17 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Shield class definition
+**********************************************************************/
+
+#include "Shield.hpp"
+
+Shield::Shield()
+{
+
+}
+
+Shield::~Shield()
+{
+
+}
