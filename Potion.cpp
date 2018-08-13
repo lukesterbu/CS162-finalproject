22f328e473de01e31af919ce61243ce10bdafcbe
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:45:16 -0400

    Create Potion.cpp

---
+++ Potion.cpp
@@ -0,0 +1,17 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Potion class declaration
+**********************************************************************/
+
+#include "Potion.hpp"
+
+Potion::Potion()
+{
+
+}
+
+Potion::~Potion()
+{
+
+}
