39c48a9fb7288752473c8b6868ead82f5951dbbd
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:39:20 -0400

    Create Character.cpp

---
+++ Character.cpp
@@ -0,0 +1,8 @@
+/***********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Abstract class Character definition
+***********************************************************************/
+
+#include "Character.hpp"
+
