a1ee413223f5d8234af7699c17f877bd9e5cbca5
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:46:14 -0400

    Create Shield.hpp

---
+++ Shield.hpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Shield class declaration
+**********************************************************************/
+
+#ifndef SHIELD_HPP
+#define SHIELD_HPP
+
+#include "Items.hpp"
+
+class Shield : public Items
+{
+	private:
+	public:
+		Shield();
+		~Shield();
+};
+
+#endif
