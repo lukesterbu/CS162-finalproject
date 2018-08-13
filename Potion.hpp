c2c3170d116f7fbd1e5a5248031d6ca822eddb77
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:44:16 -0400

    Create Potion.hpp

---
+++ Potion.hpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Potion class declaration
+**********************************************************************/
+
+#ifndef POTION_HPP
+#define POTION_HPP
+
+#include "Items.hpp"
+
+class Potion : public Items
+{
+	private:
+	public:
+		Potion();
+		~Potion();
+};
+
+#endif
