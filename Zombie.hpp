3feed200a739c1aff75e9af4a4a68634d137ceb9
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 12:04:46 -0400

    Create Zombie.hpp

---
+++ Zombie.hpp
@@ -0,0 +1,19 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description:	Zombie class declaration
+**********************************************************************/
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include "Character.hpp"
+
+class Zombie : public Character
+{
+	public:
+		Zombie();
+		~Zombie();
+};
+
+#endif
