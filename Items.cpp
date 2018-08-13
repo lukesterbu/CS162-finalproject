7a5cf86e8cc6a349721a0404169f74b0cae92ca8
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 14:26:12 -0400

    Update Items.cpp

--- Items.cpp
+++ Items.cpp
@@ -1,7 +1,7 @@
 /********************************************************************
 ** Author:	Luke Burris
 ** Date:	8/13/2018
-** Description: Items class definition
+** Description:	Items class definition
 ********************************************************************/
 
 #include "Items.hpp"
