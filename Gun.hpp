3f0f3f180448e11984054263bb428d6cf2d9e74c
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:51:07 -0400

    Create Gun.hpp

---
+++ Gun.hpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Gun class declaration
+**********************************************************************/
+
+#ifndef GUN_HPP
+#define GUN_HPP
+
+#include "Items.hpp"
+
+class Gun : public Items
+{
+	private:
+	public:
+		Gun();
+		~Gun();
+};
+
+#endif
