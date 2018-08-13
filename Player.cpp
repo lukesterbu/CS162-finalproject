e92fb109b88709f514c6e895f978b1c0678ba2e9
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 12:03:46 -0400

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,17 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description:	Player class declaration
+**********************************************************************/
+
+#include "Character.hpp"
+
+Player::Player()
+{
+
+}
+
+Player::~Player()
+{
+
+}
