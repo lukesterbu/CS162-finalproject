89bd719b1ba745feb63eea9400ea03dd62a8d5ec
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 12:05:41 -0400

    Create Zombie.cpp

---
+++ Zombie.cpp
@@ -0,0 +1,17 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description:	Zombie class declaration
+**********************************************************************/
+
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+
+}
+
+Zombie::~Zombie()
+{
+
+}
