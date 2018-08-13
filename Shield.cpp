6f9e7f1abd301b9b90ccc09012b1de189f32690d
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 16:35:29 -0400

    Update Shield.cpp

--- Shield.cpp
+++ Shield.cpp
@@ -1,17 +1,22 @@
 /**********************************************************************
 ** Author:	Luke Burris
 ** Date:	8/13/2018
-** Description: Shield class definition
+** Description:	Shield class definition
 **********************************************************************/
 
 #include "Shield.hpp"
 
 Shield::Shield()
 {
-
+	shield = 50;
 }
 
 Shield::~Shield()
 {
 
+}
+
+void Shield::use(Character* person)
+{
+	person->setShield(shield);
 }
