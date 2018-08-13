5c753fd435bc8291f88bc4a85dad12ade610b3e6
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:55:22 -0400

    Create Ammo.hpp

---
+++ Ammo.hpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description:	Ammo class declaration
+**********************************************************************/
+
+#ifndef AMMO_HPP
+#define AMMO_HPP
+
+#include "Items.hpp"
+
+class Ammo : public Items
+{
+	private:
+	public:
+		Ammo();
+		~Ammo();
+};
+
+#endif
