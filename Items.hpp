a4e81a9b27227fffc3517529d551b0f33555c5b0
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:33:53 -0400

    Create Items.hpp

---
+++ Items.hpp
@@ -0,0 +1,18 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description: Abstract Items class declaration
+**********************************************************************/
+
+#ifndef ITEMS_HPP
+#define ITEMS_HPP
+
+class Items
+{
+	private:
+	public:
+		Items();
+		~Items()
+};
+
+#endif
