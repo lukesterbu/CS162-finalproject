eb27e0fc4f14a6db84de43a5cf1819446ab780e9
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 11:57:40 -0400

    Create Cure.hpp

---
+++ Cure.hpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description:	Cure class declaration
+**********************************************************************/
+
+#ifndef CURE_HPP
+#define CURE_HPP
+
+#include "Items.hpp"
+
+class Cure : public Items
+{
+	private:
+	public:
+		Cure();
+		~Cure();
+};
+
+#endif
