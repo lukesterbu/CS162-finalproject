261b244fe8d63c722358e0421fe1caed46f6189f
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 12:02:22 -0400

    Create Player.hpp

---
+++ Player.hpp
@@ -0,0 +1,19 @@
+/**********************************************************************
+** Author:	Luke Burris
+** Date:	8/13/2018
+** Description:	Player class declaration
+**********************************************************************/
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "Character.hpp"
+
+class Player : public Character
+{
+	public:
+		Player();
+		~Player();
+};
+
+#endif
