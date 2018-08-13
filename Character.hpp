3fa2510832b25e15553d634895ec90cb5c7fe6c0
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 12:33:41 -0400

    Update Character.hpp

--- Character.hpp
+++ Character.hpp
@@ -1,22 +1,33 @@
 /**********************************************************************
 ** Author:	Luke Burris
 ** Date:	8/13/2018
-** Description:	Abstract Character class declaration
+** Description:	Character class declaration
 **********************************************************************/
 
 #ifndef CHARACTER_HPP
 #define CHARACTER_HPP
-//might want to just use character class from project3
+
+#include <string>
+
 class Character
 {
 	protected:
-		int hp;
-		int shield;
+		int atkDie; // how many attack dies
+		int defDie; // how many defense dies
+		int atkNum; // how many attack die faces
+		int defNum; // how many defense die faces
+		int shield; 
+		int hp; // hit points
+		std::string type; // used for character name
 	public:
 		Character();
 		~Character();
-		virtual void attack() = 0;
-		virtual void defend() = 0;
+		virtual int attack();
+		virtual int defense(int);
+		int getShield();
+		void setHp(int);
+		int getHp();
+		std::string getType(); // returns type
 };
 
 #endif
