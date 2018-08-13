f004969aeb69a6b2eea4cc5b8b4a2643d649b4e3
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-13 12:28:48 -0400

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -6,12 +6,71 @@
 
 #include "Character.hpp"
 
+using std::cout;
+using std::endl;
+using std::string;
+
 Character::Character()
 {
-
+	atkDie = 0;
+	defDie = 0;
+	atkNum = 0;
+	defNum = 0;
+	armor = 0;
+	strength = 0;
+	type = "Character";
 }
 
 Character::~Character()
+{}
+
+int Character::attack()
 {
+	int attack = 0;
+	for (int i = 0; i < atkDie; i++)
+	{
+		attack += rand() % (atkNum - 1 + 1) + 1;
+	}
+	return attack;
+}
+// Calculates damage done
+int Character::defense(int attack)
+{
+	int defense = 0;
+	int difference = 0;
+	for (int i = 0; i < defDie; i++)
+	{
+		defense += rand() % (defNum - 1 + 1) + 1;
+	}
+	if ((defense + armor) >= attack)
+		difference = 0;
+	else
+		difference = attack - defense - armor;
 	
+	setStrength(difference);
+	
+	return defense;
+}
+
+int Character::getArmor()
+{
+	return armor;
+}
+// Doesn't let strength go below 0
+void Character::setStrength(int difference)
+{
+	if (strength - difference < 0)
+		strength = 0;
+	else 
+		strength -= difference;
+}
+
+int Character::getStrength()
+{
+	return strength;
+}
+
+string Character::getType()
+{
+	return type;
 }
