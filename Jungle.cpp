033d757855d83307d6cf837b2432138080213acb
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-15 16:27:16 -0400

    Update Jungle.cpp

--- Jungle.cpp
+++ Jungle.cpp
@@ -4,7 +4,12 @@
 ** Description: Jungle class definition
 ******************************************************************/
 
+class Item; // forward declaration
+
+#include "Cure.hpp"
 #include "Jungle.hpp"
+
+#include <iostream>
 
 Jungle::Jungle()
 {
@@ -13,12 +18,18 @@ Jungle::Jungle()
 }
 
 Jungle::~Jungle()
-{
-
-}
-
+{}
+// has the cure
 void Jungle::event(Character* player)
 {
-
+	// if the event hasn't happened yet and the bag isn't full
+	if (events == 1 && !(player->isBagFull()))
+	{
+		events--; // decrement so event doesn't happen
+		Items* cure = new Cure;
+		cout << "You found the Cure!" << endl;
+		player->addToBag(cure);
+	}
+	else if (events == 0)
+		cout << "The event already happened here!" << endl;
 }
-
