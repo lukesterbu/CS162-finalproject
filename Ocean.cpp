6b66fedcac39da7de14ce48f315d85ffee7006fb
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-15 14:27:32 -0400

    Update Ocean.cpp

--- Ocean.cpp
+++ Ocean.cpp
@@ -13,11 +13,18 @@ Ocean::Ocean()
 }
 
 Ocean::~Ocean()
-{
-
-}
-
+{}
+// player finds a shield
 void Ocean::event(Character* player)
 {
-
+	// if the event hasn't happened yet and the bag isn't full
+	if (events == 1 && !(player->isBagFull()))
+	{
+		events--; // decrement so event doesn't happen
+		Items* shield = new Shield;
+		cout << "You found a shield!" << endl;
+		player->addToBag(shield);
+	}
+	else if (events == 0)
+		cout << "The event already happened here!" << endl;
 }
