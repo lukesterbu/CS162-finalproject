a437c36959cd57d7eb5e4aa2e78949d051faecc7
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-15 14:31:20 -0400

    Update Desert.cpp

--- Desert.cpp
+++ Desert.cpp
@@ -6,17 +6,29 @@
 
 #include "Desert.hpp"
 
+#include <iostream>
+
 Desert::Desert()
 {
 	letter = 'D';
+	events = 1;
 }
 
 Desert::~Desert()
 {
 
 }
-
+// player loses 10 health due to heat
 void Desert::event(Character* player)
 {
-
+	// if the event hasn't happened yet
+	if (events == 1)
+	{
+		events--; // decrement so event doesn't happen
+		cout << "The heat is excruciating." << endl;
+		cout << "You lose 10 health!" << endl;
+		player->setHp(player->getHp() - 10);
+	}
+	else if (events == 0)
+		cout << "The event already happened here!" << endl;
 }
